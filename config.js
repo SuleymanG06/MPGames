a05b58f3f9bae805f4ea9c6f7d9fe2baee54dbd5
SuleymanG06 <suleymangzl26@gmail.com>
2026-07-12 19:05:59 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,4 +4,4 @@
 // skor tablosu ve online TKM kapalı olur, geri kalan her şey çalışır.
 
 export const SUPABASE_URL = "https://pfzlfhminstenodxjxbu.supabase.co";      // örn: "https://abcdefgh.supabase.co"
-export const SUPABASE_ANON_KEY = "https://pfzlfhminstenodxjxbu.supabase.co"; // Project Settings → API → anon public
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBmemxmaG1pbnN0ZW5vZHhqeGJ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM4NTEzOTcsImV4cCI6MjA5OTQyNzM5N30.TASz_xbGyjGqm0QrBDimRudGsgBMkZDuOWVOl00k92A"; // Project Settings → API → anon public
